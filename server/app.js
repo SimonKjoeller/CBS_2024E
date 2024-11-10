c1112ace1f7087777ff42522147fb46c277d7031
SimonKjoeller <114286668+SimonKjoeller@users.noreply.github.com>
2024-11-10 17:01:23 +0100

    path

--- server/app.js
+++ server/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-
 const cookieParser = require("cookie-parser");
 const nodemailer = require("nodemailer");
 const userRoutes = require("./route/users");
