966e5203473a96de5bd6454c4215e12be229a5bd
SimonKjoeller <114286668+SimonKjoeller@users.noreply.github.com>
2024-11-15 12:19:20 +0100

    chat functionality

--- server/route/chat.js
+++ server/route/chat.js
@@ -14,23 +14,17 @@ chatRoutes.get("/", checkAuth, (req, res) => {
     res.sendFile(path.join(__dirname, "../../public/pages/chat.html"));
 });
 
-// Endpoint to find recipient
 chatRoutes.post("/recipient", checkAuth, (req, res) => {
     const { username } = req.body;
-    console.log("Looking for recipient:", username);
+    const query = `SELECT username FROM users WHERE username LIKE ? LIMIT 4`;
 
-    const query = `SELECT * FROM users WHERE username = ?`;
-
-    db.get(query, [username], (err, user) => {
+    db.all(query, [`%${username}%`], (err, users) => {
         if (err) {
             return res.status(500).json({ error: err.message });
         }
-        if (user) {
-            res.status(200).send({ message: "Recipient found", user: user });
-        } else {
-            res.status(404).send({ message: "Recipient not found" });
-        }
+        res.status(200).json(users);
     });
 });
+
 
 module.exports = chatRoutes;
