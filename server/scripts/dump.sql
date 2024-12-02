e3a64527f5890984dcef5bd1ba923fcd28a6499f
SimonKjoeller <114286668+SimonKjoeller@users.noreply.github.com>
2024-12-02 14:24:23 +0100

    Update dump.sql

--- server/scripts/dump.sql
+++ server/scripts/dump.sql
@@ -2,15 +2,15 @@
 INSERT INTO users
     (username, email, password, phone, verified)
 VALUES
-    ('anders', 'anders@gmail.com', '$2b$10$pXQSGfwSRTFt40i6Oziz2uEdiy5iqor9O0IKZvXzrD/mmx7A7EA.W', 1, 1);
+    ('anders', '00e642a9aeba510abfc6572c5cc96e9e3dbcbb348339b2c9030cde718dca3bd3', '$2b$10$pXQSGfwSRTFt40i6Oziz2uEdiy5iqor9O0IKZvXzrD/mmx7A7EA.W', 1, 1);
 INSERT INTO users
     (username, email, password, phone, verified)
 VALUES
-    ('hjalte', 'hjalte@gmail.com', '$2b$10$qOVztOwrJ8dyUXwQpOL8EOmHLVbvUHOm1HEptDNUL5tEwZzucQ4Py', 2, 1);
+    ('hjalte', '7e6b90c9681cbcbaa01702aa65fc7288b058a8c1fad9e6d7a13b73cb9118f3ba', '$2b$10$qOVztOwrJ8dyUXwQpOL8EOmHLVbvUHOm1HEptDNUL5tEwZzucQ4Py', 2, 1);
 INSERT INTO users
     (username, email, password, phone, verified)
 VALUES
-    ('simon', 'simon@gmail.com', '$2b$10$L1zB6LDc9KnkVjxUGUp55OzCun.IDDrlKcV3Fsl/SeG.Xo91NZvvC', 3, 1);
+    ('simon', 'e9efeb7e88502186c55f9d5f22079aee', '$2b$10$L1zB6LDc9KnkVjxUGUp55OzCun.IDDrlKcV3Fsl/SeG.Xo91NZvvC', 3, 1);
 
 
 
